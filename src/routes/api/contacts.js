24dd069509b46911f10d3db061c20fb379b979b8
Vladislav Chornyi <98426939+VladyslavIT@users.noreply.github.com>
2023-01-11 22:03:59 +0300

    add upd

---
+++ src/routes/api/contacts.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact
+} = require('../../models/contacts');
+
+router.get('/', async (req, res, next) => {
+ try {
+  const result = await listContacts();
+  res.json(result);
+ } catch (error) {
+  next(error);
+ }
+})
+
+router.get('/:contactId', async (req, res, next) => {
+ try {
+  const {id} = req.params;
+  const result = await getContactById(id);
+  res.json(result);
+ } catch (error) {
+  next(error);
+ }
+})
+
+router.post('/', async (req, res, next) => {
+  res.json({ message: 'template message' })
+})
+
+router.delete('/:contactId', async (req, res, next) => {
+  res.json({ message: 'template message' })
+})
+
+router.put('/:contactId', async (req, res, next) => {
+  res.json({ message: 'template message' })
+})
+
+module.exports = router
